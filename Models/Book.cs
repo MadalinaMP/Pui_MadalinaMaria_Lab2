2b1c3d3922f3264cd22114e5e2bff2f7f6b9e933
MĂDĂLINA-MARIA PUI <madalina.pui@stud.ubbcluj.ro>
2021-10-21 20:27:07 +0300

    laborator 4

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pui_MadalinaMaria_Lab2.Models
 {
@@ -10,7 +11,10 @@ namespace Pui_MadalinaMaria_Lab2.Models
         public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+
+        [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
         public ICollection<Order> Orders { get; set; }
+        public ICollection<PublishedBook> PublishedBooks { get; set; }
     }
 }
